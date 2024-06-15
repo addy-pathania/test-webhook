8ab420e8c7d3083ac961069c3577d107452bb8fc
Addy Pathania <aditya.pathania22@gmail.com>
2024-06-14 22:47:37 -0400

    test 10

--- scripts/teams-notification.js
+++ scripts/teams-notification.js
@@ -5,6 +5,19 @@
  */
 function transformPayload(payload) {
   console.log("payload", payload);
+
+  const title = `JSS Release ${payload.tag_name}`;
+  const releaseUrl = payload.html_url;
+  const publishedBy = payload.author.login;
+
+  const teamsPayload = {
+    "@type": "MessageCard",
+    "@context": "http://schema.org/extensions",
+    themeColor: "0076D7",
+    title: title,
+    text: `**Release:** ${releaseUrl}\n\n**Published by:** ${publishedBy}}`,
+  };
+  return teamsPayload;
 }
 
 (async () => {
@@ -14,5 +27,16 @@ function transformPayload(payload) {
     return;
   }
 
-  transformPayload(github.event);
+  try {
+    await fetch(process.env.TEAMS_WEBHOOK_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        ...transformPayload(github.event.release),
+      }),
+    });
+  } catch (error) {
+    // TODO: add better log message
+    console.log("Error occurred ", error);
+    process.exit(1);
+  }
 })();
