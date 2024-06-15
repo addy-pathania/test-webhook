2c47b10353776b6fd8f47c4352995e23fb8fff91
Addy Pathania <aditya.pathania22@gmail.com>
2024-06-14 22:24:40 -0400

    test 8

--- scripts/teams-notification.js
+++ scripts/teams-notification.js
@@ -5,18 +5,18 @@
  */
 function transformPayload(payload) {
   console.log("payload", payload);
-  const title = `JSS Release ${payload.release.body.tag_name}`;
-  const releaseUrl = payload.release.body.html_url;
-  const publishedBy = payload.sender.login;
+  // const title = `JSS Release ${payload.release.body.tag_name}`;
+  // const releaseUrl = payload.release.body.html_url;
+  // const publishedBy = payload.sender.login;
 
-  const teamsPayload = {
-    "@type": "MessageCard",
-    "@context": "http://schema.org/extensions",
-    themeColor: "0076D7",
-    title: title,
-    text: `**Release:** ${releaseUrl}\n\n**Published by:** ${publishedBy}}`,
-  };
-  return teamsPayload;
+  // const teamsPayload = {
+  //   "@type": "MessageCard",
+  //   "@context": "http://schema.org/extensions",
+  //   themeColor: "0076D7",
+  //   title: title,
+  //   text: `**Release:** ${releaseUrl}\n\n**Published by:** ${publishedBy}}`,
+  // };
+  // return teamsPayload;
 }
 
 (async () => {
@@ -26,16 +26,18 @@ function transformPayload(payload) {
     return;
   }
 
-  try {
-    await fetch(process.env.TEAMS_WEBHOOK_URL, {
-      method: "POST",
-      body: JSON.stringify({
-        ...transformPayload(github.event),
-      }),
-    });
-  } catch (error) {
-    // TODO: add better log message
-    console.log("Error occurred ", error);
-    process.exit(1);
-  }
+  transformPayload(github.event);
+
+  // try {
+  //   await fetch(process.env.TEAMS_WEBHOOK_URL, {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       ...transformPayload(github.event),
+  //     }),
+  //   });
+  // } catch (error) {
+  //   // TODO: add better log message
+  //   console.log("Error occurred ", error);
+  //   process.exit(1);
+  // }
 })();
